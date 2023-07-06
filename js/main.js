c32c2341f10608ff4b795601ae9f1a4c39d2e2e9
StephenAdeosun <stephenadeosun411@gmail.com>
2023-07-06 07:02:04 +0100

    Yes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing",{
+    strings:["Software Engineering","Product Design","Data Science","Product Marketing","Data Analysis", "Product Management", ],
+    typeSpeed:70,
+    backSpeed:70,
+    loop:true
+
+}
+)
